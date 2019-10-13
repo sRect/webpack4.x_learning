323c23ec9b9425fd6376b02248e4220629f0dbfc
srect <fcqun@sina.cn>
2019-10-13 23:53:50 +0800

    feat: :sparkles: webpack-dev-server

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,3 +1,17 @@
+const { join } = require('path');
 module.exports = {
-  mode: 'development'
+  mode: 'development',
+  devServer: {
+    contentBase: join(__dirname, '../dist'),
+    port: 8080,
+    compress: true,
+    hot: true,
+    open: true,
+    host: 'localhost',
+    historyApiFallback: true, // 该选项的作用所有的404都连接到index.html
+    proxy: {
+      // 代理到后端的服务地址，会拦截所有以api开头的请求地址
+      "/api": "http://localhost:3000"
+    }
+  }
 }
