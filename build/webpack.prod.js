8b5a75ae8e2994fce23bdee75f0099ac7ee20ba5
srect <fcqun@sina.cn>
2019-10-14 01:18:50 +0800

    feat: :sparkles: optimization优化项处理压缩css和js

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,3 +1,38 @@
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin'); // 混淆压缩js
+
 module.exports = {
-  mode: 'production'
+  mode: 'production',
+  optimization: { // 优化项
+    minimizer: [ // 放置压缩方案
+      // 压缩css(注意：配置了css压缩后，也要手动配置js的压缩，不配置js就不压缩了)
+      new OptimizeCssAssetsPlugin(), 
+      // https://www.npmjs.com/package/terser-webpack-plugin
+      new TerserPlugin({
+        test: /\.js(\?.*)?$/i,
+        exclude: /node_modules/,
+        cache: true,
+        parallel: true,
+        sourceMap: true,
+        terserOptions: {
+          ecma: undefined,
+          warnings: false,
+          parse: {},
+          compress: {
+            drop_console: true,
+            drop_debugger: true
+          },
+          mangle: true, // true/false 是否混淆变量名
+          module: false,
+          output: null,
+          toplevel: false,
+          nameCache: null,
+          ie8: false,
+          keep_classnames: undefined,
+          keep_fnames: false,
+          safari10: false,
+        },
+      }),
+    ]
+  }
 }
